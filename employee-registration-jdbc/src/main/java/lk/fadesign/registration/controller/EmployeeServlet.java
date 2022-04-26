4b74caa427bb03ba645e4668202bf11ceccd85c6
Faiyaz Rafeek <fayaznahwi@gmail.com>
2022-04-26 13:45:46 +0530

    emp registration completed

--- employee-registration-jdbc/src/main/java/lk/fadesign/registration/controller/EmployeeServlet.java
+++ employee-registration-jdbc/src/main/java/lk/fadesign/registration/controller/EmployeeServlet.java
@@ -1,6 +1,8 @@
 package lk.fadesign.registration.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import lk.fadesign.registration.model.Employee;
 /**
  * Servlet implementation class EmployeeServlet
  */
-@WebServlet("/EmployeeServlet")
+@WebServlet("/register")
 public class EmployeeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -33,6 +35,9 @@ public class EmployeeServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/employeeregister.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -60,7 +65,8 @@ public class EmployeeServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		
-		response.sendRedirect("/");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/views/employeedetails.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
--- employee-registration-jdbc/src/main/webapp/WEB-INF/views/employeeregister.jsp
+++ employee-registration-jdbc/src/main/webapp/WEB-INF/views/employeeregister.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+ pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,13 +7,37 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<div>
-		<h1>Employee Registration Form</h1>
-		<form action="<%= request.getContextPath() %>/register" method="post">
-			<table style="width: 80%">
-				<tr>tr
-			</table>
-		</form>
-	</div>
+ <div align="center">
+  <h1>Employee Register Form</h1>
+  <form action="<%= request.getContextPath() %>/register" method="post">
+   <table style="with: 80%">
+    <tr>
+     <td>First Name</td>
+     <td><input type="text" name="firstName" /></td>
+    </tr>
+    <tr>
+     <td>Last Name</td>
+     <td><input type="text" name="lastName" /></td>
+    </tr>
+    <tr>
+     <td>UserName</td>
+     <td><input type="text" name="username" /></td>
+    </tr>
+    <tr>
+     <td>Password</td>
+     <td><input type="password" name="password" /></td>
+    </tr>
+    <tr>
+     <td>Address</td>
+     <td><input type="text" name="address" /></td>
+    </tr>
+    <tr>
+     <td>Contact No</td>
+     <td><input type="text" name="contact" /></td>
+    </tr>
+   </table>
+   <input type="submit" value="Submit" />
+  </form>
+ </div>
 </body>
 </html>
